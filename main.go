bd2a57c033030dbaafe7c2212ab8ca13f5a77c8f
Ricardo Oliveira <rcoproc@gmail.com>
2019-10-21 01:11:50 -0300

    First Commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var colors map[string]string
+
+	// colors := make(map[int]string)
+	// colors[10] = "#ffffff"
+
+	// delete(colors, 10)
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	fmt.Println(colors)
+	printMap(colors)
+
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
